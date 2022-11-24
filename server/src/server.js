74e783eb328b7d5d2eb28da05fcb335a8488b0c8
NinadMaladkar <maladkar.ninad@gmail.com>
2022-11-24 15:12:43 +0100

    feat: Add login & registration feature to backend

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('../config/db');
+const userRoutes = require('../routes/userRoutes');
+const { notFound, errorHandler } = require('../middlerware/errorMiddleware');
 
 const app = express();
 
 dotenv.config();
+app.use(express.json()); // To accept JSON data
+
+app.use('/api/user', userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+connectDB();
 const port = process.env.PORT || 5000;
 
 app.listen(port, console.log(`Listening on port ${port}`));
